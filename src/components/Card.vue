84df5f29008b49f71a9fe0853cdfc8b93fd4fc83
Michel Carlos <mcarlos@littleotterhealth.com>
2022-08-11 13:02:19 -0300

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="user-card">
+    <div class="avatar">
+      <img
+        :src="`https://picsum.photos/100?imageId=${user.id}`"
+        alt="Random User Picture"
+      />
+    </div>
+    <div class="content">
+      <div class="id">{{ user.id }}</div>
+      <div class="name">{{ userName }}</div>
+      <div class="email">{{ user.email }}</div>
+      <div class="gender">{{ user.gender }}</div>
+      <div class="ip">{{ user.ip_address }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardComponent",
+  props: {
+    user: Object,
+  },
+  computed: {
+    userName() {
+      return `${this.user.first_name} ${this.user.last_name}`;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.user-card {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  box-shadow: 0 1px 2px 0 rgb(0 0 0 / 30%);
+  background-color: #fff;
+  border: none;
+  border-radius: 4.8px;
+  padding: 15px;
+  width: 300px;
+  height: 150px;
+  overflow: hidden;
+  z-index: 1;
+
+  .avatar > img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+  }
+
+  .content {
+    flex: 1;
+    padding: 16px;
+    font-size: 14px;
+    position: relative;
+
+    .id {
+      font-size: 10px;
+      font-weight: 800;
+      position: absolute;
+      top: -12px;
+      right: 0;
+    }
+
+    .name {
+      font-weight: bold;
+      margin-bottom: 6px;
+    }
+    .email {
+      font-size: 10px;
+      margin-bottom: 6px;
+    }
+    .gender {
+      margin-bottom: 6px;
+    }
+  }
+
+  &:hover {
+    box-shadow: 0 4px 8px 0 rgb(0 0 0 / 15%), 0 2px 2px -1px rgb(0 0 0 / 30%);
+    transform: translateY(-2px);
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/views/UsersView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="users">
+    <div class="header">
+      <h1>User List</h1>
+      <div class="buttons">
+        <button>Order By Id</button>
+        <button>Order By Name</button>
+        <button>Remove First</button>
+      </div>
+    </div>
+    <div class="list-wrapper">
+      <Card v-for="user in users" :key="user.id" :user="user" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+import UsersData from "../data/users.json";
+
+export default {
+  name: "HomeView",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      users: UsersData,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.users {
+  .header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #121212;
+    z-index: 2;
+    position: sticky;
+    top: 0;
+    padding: 16px;
+    margin-bottom: 16px;
+
+    h1 {
+      font-size: 16px;
+      font-weight: 800;
+      color: #fff;
+      margin-bottom: 8px;
+    }
+  }
+  .list-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
+  }
+}
+</style>
